fcd1161710a8a98cb99a48c992a54389a08cc2fa
L1UK3 <lukeenness@btinternet.com>
2021-06-01 21:18:33 +0100

    commit on master

---
+++ Execute.java
@@ -0,0 +1,30 @@
+import javax.tools.JavaCompiler;
+
+/**
+ * Exercise on working with executing java code from a string and text file
+ * <p>
+ * This class includes functions to:
+ * <ul>
+ * <li>Read the text file
+ * <li>Convert the text file to a readable string
+ * <li>Execute the file and return outputs
+ * 
+ * <p>
+ * Research into using BeanShell?
+ * Could also use the  compiler module included with java, not sure if that would be too confusing
+ * <p>
+ * 
+ * @author  Luke Enness
+ * @version 1
+ * @since   1.0
+ */
+
+public class Execute {
+    
+    // read text file?
+
+    // convert text file to readable string
+
+    // work with execuing the file and returning output
+    
+}
